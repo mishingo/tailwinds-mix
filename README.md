2ec34733f8110dc9691104384db0231e9ecbb523
mishingo <mr@mishingo.com>
2018-05-06 20:56:57 -0400

    add base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Materialize laravel-mix
+A Front End Boilerplate for building static sites using Laravel-Mix and Materialize Css
+
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+
+var danielsFriends = ["erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey","erica","luis","mickey"];
+
+
+for(var i = 0; i < danielsFriends.length; i++ ){
+    console.log(danielsFriends[i]);
+}
+
